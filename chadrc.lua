992042f89da45b7468a18985289b0e20cc2d701b
fgbyte <fer.garciabayon@gmail.com>
2023-10-21 17:20:52 -0400

    add hover tooltip space + h[update]

--- chadrc.lua
+++ chadrc.lua
@@ -8,7 +8,7 @@ M.mappings = require "custom.mappings"
 -- Esto hace que la shell por default sea 'zsh'
   vim.opt.shell = "/bin/zsh"
 
--- Hover expicativo con F1
+-- Hover expicativo con <leader>h
 vim.api.nvim_set_keymap("n", "<leader>h", "<cmd>lua vim.lsp.buf.hover()<CR>", {silent = true})
 
 return M
